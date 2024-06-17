35f855e293232215b9d5e43270baa6e3a6d30357
MFunction96 <mfunction96@outlook.com>
2024-06-17 16:34:52 +0800

    完成基本发布脚本编写

--- .azurepipelines/publish/publish.ps1
+++ .azurepipelines/publish/publish.ps1
@@ -1,14 +1,15 @@
+param(
+	[Parameter(Mandatory = $true)]
+	[string] $OutputFolder
+)
+
 $workFolder = [System.IO.Path]::Combine([System.IO.Path]::GetTempPath(), [Guid]::NewGuid().ToString())
 $publishFolder = [System.IO.Path]::Combine($workFolder, 'publish')
-$installerFolder = [System.IO.Path]::Combine($workFolder, 'Installer')
 dotnet publish --output $publishFolder
-7z.exe a -r -t7z -m0=lzma -mx=9 -mfb=64 -md=32m -ms=on "$installerFolder/KorabliChsMod.7z" "$publishFolder/*"
-Copy-Item -Path 'C:\Program Files\7-Zip\7z.sfx' -Destination $installerFolder
-Copy-Item -Path '.\.azurepipelines\publish\config.txt' -Destination $installerFolder
-Set-Location $installerFolder
-copy /b 7z.sfx + config.txt + KorabliChsMod.7z KorabliChsModSetup.exe
+Compress-Archive -Path "$publishFolder/*" -DestinationPath "$outputFolder/KorabliChsMod.zip" -CompressionLevel Optimal -Force
+Copy-Item -Path "$publishFolder/*" -Destination $outputFolder -Recurse -Force
 try {
-  # Remove-Item -Path $workFolder-Force -Recurse
+  Remove-Item -Path $workFolder -Force -Recurse
 } catch {
 
 }
