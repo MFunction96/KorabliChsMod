1c452e9922375712d0e3e4e8311f78c9f48659bd
MFunction96 <mfunction96@outlook.com>
2024-07-11 19:23:33 +0800

    添加日志文件大小约束为48MB

--- test/KorabliChsModTests/GameDetectorTest.cs
+++ test/KorabliChsModTests/GameDetectorTest.cs
@@ -1,11 +1,23 @@
+using Microsoft.Extensions.Logging;
+using Moq;
+using System;
+using System.Threading.Tasks;
+using Xanadu.KorabliChsMod.Core;
+
 namespace Xanadu.KorabliChsModTests
 {
     [TestClass]
     public class GameDetectorTest
     {
+        private readonly Mock<ILogger<GameDetector>> _mockLogger = new();
+
         [TestMethod]
-        public void TestMethod1()
+        public async Task VersionDetectTest()
         {
+            var gameDetector = new GameDetector(this._mockLogger.Object);
+            await gameDetector.Load(Environment.CurrentDirectory);
+
+            Assert.AreEqual("13.6.0.0.8601080", gameDetector.Version);
         }
     }
 }
