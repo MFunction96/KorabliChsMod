40d9f6d657f96e8a27fdaa9bc8177534f7dec18c
MFunction96 <mfunction96@outlook.com>
2024-07-12 22:35:38 +0800

    修正curl行为

--- .azurepipelines/publish/publish.ps1
+++ .azurepipelines/publish/publish.ps1
@@ -21,9 +21,9 @@ Copy-Item -Path "./.azurepipelines/publish/KorabliChsModInstaller.exe" -Destinat
 $hash = Get-FileHash -Path "$outputFolder/KorabliChsModInstaller.exe" -Algorithm SHA256
 $hash.Hash | Out-File "$outputFolder/KorabliChsModInstaller.exe.sig" -Encoding utf8 -Force
 if ([string]::IsNullOrEmpty($Proxy)) {
-	curl -L -o $publishFolder/windowsdesktop-runtime-win-x64.zip https://aka.ms/dotnet/8.0/windowsdesktop-runtime-win-x64.zip -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36 Edge/16.16299" --connect-timeout 60 --max-time 60 --retry 5 --retry-delay 5 --retry-max-time 60
+	curl -sSL -o $publishFolder/windowsdesktop-runtime-win-x64.zip https://aka.ms/dotnet/8.0/windowsdesktop-runtime-win-x64.zip -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36 Edge/16.16299" --connect-timeout 60 --max-time 60 --retry 5 --retry-delay 5 --retry-max-time 60
 } else {
-	curl -L -o $publishFolder/windowsdesktop-runtime-win-x64.zip https://aka.ms/dotnet/8.0/windowsdesktop-runtime-win-x64.zip -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36 Edge/16.16299" --connect-timeout 60 --max-time 60 --retry 5 --retry-delay 5 --retry-max-time 60 --proxy $Proxy
+	curl -sSL -o $publishFolder/windowsdesktop-runtime-win-x64.zip https://aka.ms/dotnet/8.0/windowsdesktop-runtime-win-x64.zip -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36 Edge/16.16299" --connect-timeout 60 --max-time 60 --retry 5 --retry-delay 5 --retry-max-time 60 --proxy $Proxy
 }
 
 makensis.exe /X"SetCompressor /SOLID /FINAL lzma" /DSOURCE="$publishFolder" /DVERSION="$env:BIN_VER" /INPUTCHARSET UTF8 /OUTPUTCHARSET UTF8 .\.azurepipelines\publish\installer_runtime.nsi
