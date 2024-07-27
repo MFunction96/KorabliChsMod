a9318815f2790a2b33e99a635c29ebb88c239d12
MFunction96 <mfunction96@outlook.com>
2024-07-27 14:06:24 +0800

    修正.NET 8安装异常

--- .azurepipelines/publish/publish.ps1
+++ .azurepipelines/publish/publish.ps1
@@ -23,9 +23,9 @@ $hash = Get-FileHash -Path "$outputFolder/KorabliChsModInstaller.exe" -Algorithm
 Write-Output $hash.Hash | Out-File -FilePath "$outputFolder/KorabliChsModInstaller.exe.sha256" -Encoding utf8 -Force
 
 if ([string]::IsNullOrEmpty($Proxy)) {
-	curl -sL -o $publishFolder/windowsdesktop-runtime-win-x64.zip https://aka.ms/dotnet/8.0/windowsdesktop-runtime-win-x64.zip -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36 Edge/16.16299" --connect-timeout 60 --max-time 60 --retry 5 --retry-delay 5 --retry-max-time 60
+	curl -sL -o $publishFolder/windowsdesktop-runtime-win-x64.exe https://aka.ms/dotnet/8.0/windowsdesktop-runtime-win-x64.exe -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36 Edge/16.16299" --connect-timeout 60 --max-time 60 --retry 5 --retry-delay 5 --retry-max-time 60
 } else {
-	curl -sL -o $publishFolder/windowsdesktop-runtime-win-x64.zip https://aka.ms/dotnet/8.0/windowsdesktop-runtime-win-x64.zip -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36 Edge/16.16299" --connect-timeout 60 --max-time 60 --retry 5 --retry-delay 5 --retry-max-time 60 --proxy $Proxy
+	curl -sL -o $publishFolder/windowsdesktop-runtime-win-x64.exe https://aka.ms/dotnet/8.0/windowsdesktop-runtime-win-x64.exe -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36 Edge/16.16299" --connect-timeout 60 --max-time 60 --retry 5 --retry-delay 5 --retry-max-time 60 --proxy $Proxy
 }
 
 makensis.exe /X"SetCompressor /SOLID /FINAL lzma" /DSOURCE="$publishFolder" /DVERSION="$env:BIN_VER" /INPUTCHARSET UTF8 /OUTPUTCHARSET UTF8 .\.azurepipelines\publish\installer_runtime.nsi
