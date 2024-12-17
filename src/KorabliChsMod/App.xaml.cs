1a1cd3a50cf19e374749be76c7633a37d6d6034e
MFunction96 <mfunction96@outlook.com>
2024-12-17 14:27:50 +0800

    采用Prism作为MVVM组件

--- src/KorabliChsMod/App.xaml.cs
+++ src/KorabliChsMod/App.xaml.cs
@@ -1,51 +1,46 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
+﻿using HandyControl.Themes;
 using Serilog;
-using System;
 using System.Text;
 using System.Windows;
-using Xanadu.KorabliChsMod.Core;
+using System.Windows.Media;
 using Xanadu.KorabliChsMod.Core.Config;
-using Xanadu.KorabliChsMod.DI;
-using Xanadu.Skidbladnir.IO.File.Cache;
 
-#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
-
-namespace KorabliChsMod
+namespace Xanadu.KorabliChsMod
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
-        public static IServiceProvider ServiceProvider { get; private set; }
-
-        public App()
+        protected override void OnStartup(StartupEventArgs e)
         {
-            var services = new ServiceCollection();
-            // 注册日志组件
-            services.AddLogging(configuration => configuration.AddConsole().AddSerilog());
-            // 注册日志文件输出，48MB大小限制
-            services.AddSerilog(configuration => configuration
+            Log.Logger = new LoggerConfiguration()
                 .Enrich.FromLogContext()
-                .WriteTo.File(IKorabliFileHub.LogFilePath, encoding: Encoding.UTF8, fileSizeLimitBytes: 50331648));
-            // 注册配置文件
-            services.AddSingleton<IKorabliFileHub, KorabliFileHub>();
-            // 注册游戏探查服务
-            services.AddSingleton<IGameDetector, GameDetector>();
-            // 注册Lesta Game Center探查服务
-            services.AddSingleton<ILgcIntegrator, LgcIntegrator>();
-            // 注册网络引擎服务
-            services.AddSingleton<INetworkEngine, NetworkEngine>();
-            // 注册缓存池服务
-            services.AddSingleton<IFileCachePool, FileCachePool>();
-            // 注册更新助理服务
-            services.AddSingleton<IUpdateHelper, UpdateHelper>();
-            // 注册Mod安装服务
-            services.AddSingleton<IModInstaller, ModInstaller>();
-            
-            App.ServiceProvider = services.BuildServiceProvider();
+                .WriteTo.File(IKorabliFileHub.LogFilePath, encoding: Encoding.UTF8, fileSizeLimitBytes: 50331648)
+                .CreateLogger();
+
+            base.OnStartup(e);
+            var boot = new Bootstrapper();
+            boot.Run();
+        }
+
+        internal void UpdateTheme(ApplicationTheme theme)
+        {
+            if (ThemeManager.Current.ApplicationTheme != theme)
+            {
+                ThemeManager.Current.ApplicationTheme = theme;
+            }
+        }
+
+        internal void UpdateAccent(Brush accent)
+        {
+            if (ThemeManager.Current.AccentColor != accent)
+            {
+                ThemeManager.Current.AccentColor = accent;
+            }
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            Log.CloseAndFlush();
+            base.OnExit(e);
         }
     }
-
 }
--- src/KorabliChsMod/Bootstrapper.cs
+++ src/KorabliChsMod/Bootstrapper.cs
@@ -1,10 +1,40 @@
-﻿using Stylet;
-using KorabliChsMod.ViewModels;
+﻿using Prism.DryIoc;
+using Prism.Ioc;
+using Serilog;
+using System.Windows;
+using Xanadu.KorabliChsMod.Core;
+using Xanadu.KorabliChsMod.Core.Config;
+using Xanadu.KorabliChsMod.DI;
+using Xanadu.KorabliChsMod.Views;
+using Xanadu.Skidbladnir.IO.File.Cache;
 
-namespace KorabliChsMod
+namespace Xanadu.KorabliChsMod
 {
-    public class Bootstrapper : Bootstrapper<RootViewModel>
+    public class Bootstrapper : PrismBootstrapper
     {
+        protected override DependencyObject CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
 
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            // 注册日志服务
+            containerRegistry.RegisterInstance(Log.Logger);
+            // 注册配置文件
+            containerRegistry.RegisterSingleton<IKorabliFileHub, KorabliFileHub>();
+            // 注册游戏探查服务
+            containerRegistry.RegisterSingleton<IGameDetector, GameDetector>();
+            // 注册Lesta Game Center探查服务
+            containerRegistry.RegisterSingleton<ILgcIntegrator, LgcIntegrator>();
+            // 注册网络引擎服务
+            containerRegistry.RegisterSingleton<INetworkEngine, NetworkEngine>();
+            // 注册缓存池服务
+            containerRegistry.RegisterSingleton<IFileCachePool, FileCachePool>();
+            // 注册更新助理服务
+            containerRegistry.RegisterSingleton<IUpdateHelper, UpdateHelper>();
+            // 注册Mod安装服务
+            containerRegistry.RegisterSingleton<IModInstaller, ModInstaller>();
+        }
     }
 }
